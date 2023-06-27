fafa6045af016cdbb46c2a0aac4791855f2ea44b
Nicholas Sielicki <nix@opensource.nslick.com>
2023-06-27 07:21:31 -0700

    feat: add more packages to hm

---
+++ cells/personal/homeProfiles/base/home/packages.nix
@@ -0,0 +1,19 @@
+{inputs, ...}:
+with inputs.nixpkgs; [
+  bashInteractive
+  fd
+  ripgrep
+  rsync
+  tree
+  watch
+  wget
+  curl
+  htop
+  silver-searcher
+  parallel
+  bitwise
+  ack
+  aria2
+  httpie
+  youtube-dl
+]
